45aa632e38647bf62628dae21abc82c1a554e461
Kunal Pandit <kpandit@nygenome.org>
2020-03-16 12:03:17 -0400

    remove unused files

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup, find_packages
 from os import path
 
+here = path.abspath(path.dirname(__file__))
+
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='PySeq',
+    name='pyseq2500',
     version='0.1',
-    url='https://github.com/nygctech/PySeq2500V2',
+    description='Control an Illumina HiSeq 2500 System',
+    long_description=long_description,
+    url='https://github.com/nygctech/PySeq2500',
     author='Kunal Pandit',
+    license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        #'License ::' ADD LICENSE
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3'
+        'Operating System :: Microsoft :: Windows',
         ],
     keywords='sequencing, HiSeq, automation, biology',
-    package_dir={'':: 'src'},
+    packages=['pyseq'],
     python_requires='>=3.5',
     install_requires=['serial', 'numpy', 'scipy', 'imageio'],
     #package_data={ }, Add data files inside of package
@@ -24,5 +30,9 @@ setup(
     #    'sample': ['package_data.dat'], ## add data files inside of package
     #},
     #entry_points ???
-    #project_urls={ ADD HACKTERIA},
+    project_urls={
+        'Bug Reports': 'https://github.com/nygctech/PySeq2500/issues',
+        'Support':'https://www.hackteria.org/wiki/HiSeq2000_-_Next_Level_Hacking#Control_Software',
+        'Source': 'https://github.com/nygctech/PySeq2500'
+        },
 )
